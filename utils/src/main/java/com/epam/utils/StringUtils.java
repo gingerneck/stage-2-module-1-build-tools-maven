261c8b99c3a444447da735053b9a0a621abd5b2e
Nikita Pyshchev <npyshchev@igatec.com>
2024-05-10 22:49:15 +0300

    wip

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,8 +4,11 @@ public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         boolean res = false;
         if (str != null && !str.isEmpty()) {
-            if (str.charAt(0) != '-') {
+            if (str.charAt(0) != '-' && str.charAt(0) != ' ') {
                 res = true;
+            }
+            if(str.equals("0")){
+                res = false;
             }
         }
         return res;
